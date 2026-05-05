a7bae3229636d565fe9a759641f065e08ca69e17
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-05-05 20:57:15 +0100

    update

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+document.write("Hello, World!");
+
+var A = "This is a string";
+
+document.write(A);
+
+var message = "Hello, World!";
+window.alert(message);
+
+var B = "Concatenated" + "String"
+document.write(B)
+
+var Superheroes = "Avengers",
+    IronMan = "Tony Stark",
+    CaptainAmerica = "Steve Rogers",
+    Hulk = "Bruce Banner";
+
+document.write(IronMan);
+
+document.write("To quote: \"This is a quote\"");
+
+document.write("\"This is another quote,"
++ "which is very quotable "
++ "- The Quoter\"");
+
+"Nick" + " is learning Javascript" // this is a comment on my coding 
