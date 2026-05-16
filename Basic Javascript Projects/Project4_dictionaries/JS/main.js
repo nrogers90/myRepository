a9c8d9ce685819fd3c8ace5f48fdb3ebcb483318
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-05-16 11:43:38 +0100

    added notes to 'delete' assignment

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -1,4 +1,6 @@
 function my_Dictionary() {
+
+    // dictionary:
     var Bounty_Hunters = {
         Name: "Bossk",
         Species: "Trandoshian",
@@ -6,7 +8,10 @@ function my_Dictionary() {
         Episode: 5
     };
 
+    // delete bounty hunter's name
     delete Bounty_Hunters.Name;
 
+
+    // display the deleted value n HTML
     document.getElementById("Dictionary").innerHTML = Bounty_Hunters.Name;
 }
