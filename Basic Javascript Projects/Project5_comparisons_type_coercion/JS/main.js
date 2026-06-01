f6ae365c34e4dc52cb4f9d6665a72c9c5ebeadb7
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-06-01 20:20:31 +0100

    assignment update

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -37,5 +37,55 @@ console.log(2 + 2);
 console.log(10 < 2);
 
 document.write(10 == 10);
-<br>
+document.write("<br>");
 document.write(3 == 11);
+
+// true 
+X = 5;
+Y = 5;
+document.write(X === Y);
+
+// false_Function
+X = 5;
+Y = "5";
+document.write(X === Y);
+
+// false - different data type and different value
+X = 5;
+Y = "10";
+document.write(X === Y);
+
+// false - same data type but different value
+A = "Nick";
+B = "Nicholas";
+document.write(A === B);
+
+document.write("<br>");
+
+// AND Operator - true
+document.write(10 > 2 && 10 > 4);
+
+document.write("<br>");
+
+// AND Operator - false
+document.write(5 > 10 && 6 > 7);
+
+document.write("<br>");
+
+// OR operator - true
+document.write( 5 > 10 || 20 > 10);
+
+document.write("<br>");
+
+// OR Operator - false
+document.write( 5 > 10 || 10 > 20);
+
+// NOT Operator
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (20 > 10);
+}
+
+// NOT Operator - Double Negative
+function not_Funtion_Double() {
+    document.getElementById("Not2").innerHTML = ! (5 > 10)
+}
