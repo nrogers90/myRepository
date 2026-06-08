de80e53bea2a0f47271fb8cff63aa8a0aa9406ef
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-06-08 20:38:07 +0100

    assignment updates

---
+++ Basic Javascript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,13 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function Vote_Function() {
+    var Age, Can_Vote;
+    Age = document.getElementById("Age").value;
+    Can_Vote = (Age < 18) ? "You are too young" : "You are old enough";
+    document.getElementById("Vote").innerHTML = Can_Vote + " to vote.";
+}
