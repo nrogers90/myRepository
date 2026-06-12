aed843b00ad9483d2e9c4cc56e25cd694f8ce438
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-06-12 19:36:45 +0100

    project 6 upload

--- Basic Javascript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic Javascript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,3 +1,5 @@
+
+// funtion with HTML and JS using a ternart operation
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -5,9 +7,39 @@ function Ride_Function() {
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
+// Constructor function utilising "new" and "this keywords"
 function Vote_Function() {
     var Age, Can_Vote;
     Age = document.getElementById("Age").value;
     Can_Vote = (Age < 18) ? "You are too young" : "You are old enough";
     document.getElementById("Vote").innerHTML = Can_Vote + " to vote.";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make; 
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+// Funtion displaying the reuslts of the constructor with a HTML element
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
+
+//Nested Function 
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
 }
