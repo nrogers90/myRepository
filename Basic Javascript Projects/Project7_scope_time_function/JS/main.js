a79a84b9191276e5139018eef69c2c22abc98b21
nickrogers90 <nicholasphiliprogers@gmail.com>
2026-06-14 16:52:01 +0100

    project 7 assignment upload

--- Basic Javascript Projects/Project7_scope_time_function/JS/main.js
+++ Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -21,3 +21,38 @@ function Add_numbers_2() {
 }
 Add_numbers_1();
 Add_numbers_2();
+
+// Get Date 'If' Statement
+function get_Date() {
+    if (new Date().getHours() <18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+// Age 'Else' Statement
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+// Else If Statement
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
